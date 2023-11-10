237e028ed2052110e241da9acda0326e12f5cf94
Sergiy <sergiy.redko@ucalgary.ca>
2023-11-10 08:28:49 +0000

    HSS? Maybe?

--- prefixscan.c
+++ prefixscan.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <math.h>
 
+// Prints out an int array. Used for diagnostics.
+void printOutArray(int* ints, int size){
+    for(int i = 0; i < size; i++){
+        printf("%d ", ints[i]);
+    }
+    printf("\n");
+}
 
 // Parse a vector of integers from a file, one per line.
 // Return the number of integers parsed.
-int parse_ints(FILE *file, int **ints) {
+int parseInts(FILE *file, int **ints) {
     
     if (file == NULL) {
         return -1; 
@@ -45,7 +54,7 @@ int parse_ints(FILE *file, int **ints) {
 
 
 // Write a vector of integers to a file, one per line.
-void write_ints(FILE *file, int *ints, int size) {
+void writeInts(FILE *file, int *ints, int size) {
     // Check if the file is valid
     if (file == NULL) {
         fprintf(stderr, "Invalid file pointer\n");
@@ -59,34 +68,67 @@ void write_ints(FILE *file, int *ints, int size) {
 }
 
 
-// Return the result of a sequential prefix scan of the given vector of integers.
+// Returns the result of a sequential prefix scan of the given vector of integers.
 int* SEQ(int *ints, int size) {
     
-    //Initialize new vector
-    int* new_vector = (int*)malloc(sizeof(int)*size);
+    // Initialize new vector.
+    int* newVector = (int*)malloc(sizeof(int)*size);
 
-    new_vector[0] = ints[0];
+    newVector[0] = ints[0];
 
-    //Each output vector index is the previous index plus the next number in the input
+    // Each output vector index is the previous index plus the next number in the input.
     for(int i = 1; i < size; i++){
-        new_vector[i] = new_vector[i-1] + ints[i];
+        newVector[i] = newVector[i-1] + ints[i];
         
     }
-    //return output vector
-    return new_vector;
-
+    
+    return newVector;
 }
 
+// Performs a single Hillis/Steele step.
+void hillisSteeleStep(int* ints, int size, int stepNum){
+    int stepSize = round(pow(2, stepNum));
 
-// Return the result of Hillis/Steele, but with each pass executed sequentially
+    // printOutArray(ints, size);
+
+    for(int i = size - 1; i >= stepSize; i--){
+        ints[i] = ints[i] + ints[i - stepSize];
+    }
+
+    // printOutArray(ints, size);
+    // printf("\n");
+}
+
+// Return the result of Hillis/Steele, but with each pass executed sequentially.
 int* HSS(int *ints, int size) {
+    // Allocate memory for the result array.
+    int * resultArray = malloc(sizeof(int) * size);
+    // Copy ints into result array.
+    for(int i = 0; i < size; i++){
+        resultArray[i] = ints[i];
+    }
 
+    // Calculate the number of Hillis/Steele steps to perform.
+    int numOps = round(log(size) / log(2));
+
+    // Perform each step.
+    for(int i = 0; i < numOps; i++){
+        hillisSteeleStep(resultArray, size, i);
+    }
+
+    return resultArray;
 }
 
 
 // Return the result of Hillis/Steele, parallelized using pthread
 int* HSP(int *ints, int size, int numthreads) {
+    int * newVector = (int*)malloc(sizeof(int)*size);
 
+    // TODO: Implement.
+
+
+
+    return newVector;
 }
 
 
@@ -97,14 +139,14 @@ int main(int argc, char** argv) {
         return 1;
     }
 
-    char *mode = argv[1];
-    int num_threads = atoi(argv[2]);
+    char *mode = argv[1]; // SEQ or HSS (HS Sequential) or HSP (HS Parallel).
+    int numThreads = atoi(argv[2]); // Only used in HSP.
     FILE *input = fopen(argv[3], "r");
     FILE *output = fopen(argv[4], "w");
 
     int *ints;
     int size;
-    size = parse_ints(input, &ints);
+    size = parseInts(input, &ints);
 
     int *result;
     if (strcmp(mode, "SEQ") == 0) {
@@ -112,13 +154,13 @@ int main(int argc, char** argv) {
     } else if (strcmp(mode, "HSS") == 0) {
         result = HSS(ints, size);
     } else if (strcmp(mode, "HSP") == 0) {
-        result = HSP(ints, size, num_threads);
+        result = HSP(ints, size, numThreads);
     } else {
         printf("Unknown mode: %s\n", mode);
         return 1;
     }
 
-    write_ints(output, result, size);
+    writeInts(output, result, size);
     fclose(input);
     fclose(output);
     return 0;
