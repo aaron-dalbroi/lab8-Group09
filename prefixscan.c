636cf7f76a2ec2c83db0e98552de553979ffae1f
Sergiy <sergiy.redko@ucalgary.ca>
2023-11-11 07:41:21 +0000

    All the Bounds

--- prefixscan.c
+++ prefixscan.c
@@ -3,6 +3,18 @@
 #include <string.h>
 #include <pthread.h>
 #include <math.h>
+#include <assert.h>
+
+// Struct to hold parameters for the thread function
+struct ThreadArgs {
+    int *resultArray;
+    int *swapBuffer;
+    int start;
+    int end;
+    int stepSize;
+};
+
+pthread_mutex_t HSLock;
 
 // Prints out an int array. Used for diagnostics.
 void printOutArray(int* ints, int size){
@@ -72,29 +84,31 @@ void writeInts(FILE *file, int *ints, int size) {
 int* SEQ(int *ints, int size) {
     
     // Initialize new vector.
-    int* newVector = (int*)malloc(sizeof(int)*size);
+    int* resultArray = (int*)malloc(sizeof(int)*size);
 
-    newVector[0] = ints[0];
+    resultArray[0] = ints[0];
 
     // Each output vector index is the previous index plus the next number in the input.
     for(int i = 1; i < size; i++){
-        newVector[i] = newVector[i-1] + ints[i];
+        resultArray[i] = resultArray[i-1] + ints[i];
         
     }
     
-    return newVector;
+    return resultArray;
 }
 
 // Performs a single Hillis/Steele step.
 void hillisSteeleStep(int* ints, int size, int stepNum){
     int stepSize = round(pow(2, stepNum));
 
+    // DIAGNOSTIC: ___
     // printOutArray(ints, size);
 
     for(int i = size - 1; i >= stepSize; i--){
         ints[i] = ints[i] + ints[i - stepSize];
     }
 
+    // DIAGNOSTIC: ___
     // printOutArray(ints, size);
     // printf("\n");
 }
@@ -119,19 +133,127 @@ int* HSS(int *ints, int size) {
     return resultArray;
 }
 
+// Thread function.
+void *hillisStelleThread(void *args) {
+    struct ThreadArgs *threadArgs = (struct ThreadArgs *)args;
 
-// Return the result of Hillis/Steele, parallelized using pthread
-int* HSP(int *ints, int size, int numthreads) {
-    int * newVector = (int*)malloc(sizeof(int)*size);
+    for(int i = threadArgs->end; i >= threadArgs->start; i--){
+        if(i >= threadArgs->stepSize){
+            threadArgs->swapBuffer[i] = threadArgs->resultArray[i] + threadArgs->resultArray[i-threadArgs->stepSize];
+        }
+        else{
+            threadArgs->swapBuffer[i] = threadArgs->resultArray[i];
+        }
+        
+    }
+    pthread_exit(NULL);
+}
 
-    // TODO: Implement.
+// Performs a single Hillis/Steele step in parallel. 
+void hillisSteeleParallelStep(  int ** resultArray,
+                                int ** swapBuffer,
+                                int size,
+                                int stepNumber,
+                                pthread_t * threads,
+                                struct ThreadArgs threadArgs[],
+                                int numThreads)
+{
 
+    int stepSize = round(pow(2, stepNumber));
 
+    // DIAGNOSTIC: ___
+    // printf("Before step %d:\n", stepNumber);
+    // printOutArray(*resultArray, size);
+    // printOutArray(*swapBuffer, size);
 
-    return newVector;
+    // Spawn threads.
+    for(int i = 0; i < numThreads; i++){
+        if(threadArgs[i].start != -1){
+            // Start and end always stay the same.
+            threadArgs[i].resultArray = *resultArray;
+            threadArgs[i].swapBuffer = *swapBuffer;
+            threadArgs[i].stepSize = stepSize;
+
+            pthread_create(&(threads[i]), NULL, hillisStelleThread, (void *)&(threadArgs[i]));
+        }
+    }
+
+    // Wait for threads to complete.
+    for(int i = 0; i < numThreads; i++){
+        if(threadArgs[i].start != -1){
+            pthread_join(threads[i], NULL);
+        }
+    }
+
+    // DIAGNOSTIC: ___
+    // printf("After step %d:\n", stepNumber);
+    // printOutArray(*resultArray, size);
+    // printOutArray(*swapBuffer, size);
+
+    // Swap resultArray and swapBuffer.
+    int * temp = * resultArray;
+    *resultArray = *swapBuffer;
+    *swapBuffer = temp;
+
+    // DIAGNOSTIC: ___
+    // printf("After swap: %d:\n", stepNumber);
+    // printOutArray(*resultArray, size);
+    // printOutArray(*swapBuffer, size);
+    // printf("\n");
 }
 
 
+// Return the result of Hillis/Steele, parallelized using pthread
+int* HSP(int *ints, int size, int numThreads) {
+    // Initialize storage.
+    int * resultArray = (int*)malloc(sizeof(int) * size);
+    int * swapBuffer = (int*)malloc(sizeof(int) * size); // This gets used to avoid sequencing issues with threads.
+
+    // Initialize mutex lock.
+    int rc = pthread_mutex_init(&HSLock, NULL);
+    assert(rc == 0);
+
+    // Copy ints into result array.
+    for(int i = 0; i < size; i++){
+        resultArray[i] = ints[i];
+        swapBuffer[i] = ints[i];
+    }
+
+    // Initialize array of threads.
+    pthread_t threads[numThreads];
+
+    // Calculate the number of Hillis/Steele steps to perform.
+    int numOps = round(log(size) / log(2));
+
+    // Create thread args array.
+    struct ThreadArgs threadArgs[numThreads];
+    // Calculate starts and ends for each thread.
+    // Figure out the ranges of calculation for each thread.
+    int numItemsForEachThread = (size + numThreads -1) / numThreads; // Rounded up.
+    threadArgs[0].start = 0;
+    threadArgs[0].end = numItemsForEachThread - 1;
+    for(int i = 1; i < numThreads; i++){
+        threadArgs[i].start = threadArgs[i-1].end + 1;
+        threadArgs[i].end = threadArgs[i].start + numItemsForEachThread - 1; // -1 because start and end are inclusive.
+        if(threadArgs[i].start >= size || threadArgs[i].start == -1){ // No work for this thread.
+            threadArgs[i].start = -1;
+            threadArgs[i].end = -1;
+        }
+        else if(threadArgs[i].end >= size){ // Last thread to get any work.
+            threadArgs[i].end = size - 1;
+        }
+    }
+
+    // Perform each step.
+    for(int i = 0; i < numOps; i++){
+        hillisSteeleParallelStep(&resultArray, &swapBuffer, size, i, threads, threadArgs, numThreads);
+    }
+
+    free(swapBuffer);
+    return resultArray;
+}
+
+// Entry point to the program.
 int main(int argc, char** argv) {
 
     if ( argc != 5 ) {
@@ -163,5 +285,6 @@ int main(int argc, char** argv) {
     writeInts(output, result, size);
     fclose(input);
     fclose(output);
+    free(result);
     return 0;
 }
